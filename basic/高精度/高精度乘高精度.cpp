4c1e061854340f518c54327dde266312f966333e
ZhuofanLi <z7074836@gmail.com>
2026-04-30 14:37:44 +0800

    修改错误

--- basic/高精度/高精度乘高精度.cpp
+++ basic/高精度/高精度乘高精度.cpp
@@ -6,7 +6,7 @@ vector<int>mul(vector<int>&A,vector<int>&B)
   for(int i=0;i<A.size();i++)
     for(int j=0;j<B.size();j++)
       {
-        C[i+j]=A[i]*B[j];
+        C[i+j]+=A[i]*B[j];
       }
   //统一处理进位
   int t=0;
@@ -28,6 +28,6 @@ int main()
   for(int i=a.size()-1;i>=0;i--) A.push_back(a[i]-'0');
   for(int i=b.size()-1;i>=0;i--) B.push_back(b[i]-'0');
   auto C=mul(A,B);
-  for(int i=C.size(),i>=0;i--) cout<<C[i];
+  for(int i=C.size()-1;i>=0;i--) cout<<C[i];
   return 0;
 }
