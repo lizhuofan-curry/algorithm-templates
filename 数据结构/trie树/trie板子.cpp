487d854b85222d8945d1001a87d8a5eaec18b158
ZhuofanLi <z7074836@gmail.com>
2026-05-07 19:30:45 +0800

    Create trie板子.cpp

---
+++ 数据结构/trie树/trie板子.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e5+10;
+int son[N][26],cnt[N],idx;
+void insert(string s)
+{
+  int k=0;
+  for(int i=0;s[i];i++)
+    {
+      int u=s[i]-'a';
+      if(!son[k][u]) son[k][u]=++idx;
+      k=son[k][u];
+    }
+  cnt[k]++;
+}
+int query(string s)
+{
+  int k=0;
+  for(int i=0;s[i];i++)
+    {
+      int u=s[i]-'a';
+      if(!son[k][u]) return 0;
+      k=son[k][u];
+    }
+  return cnt[k];
+}
+int main()
+{
+  int n;
+  cin>>n;
+  while(n--)
+    {
+      char op;
+      string s;
+      cin>>op>>s;
+      if(op=='I') insert(s);
+      if(op=='Q') cout<<query(s)<<"\n";
+    }
+  return 0;
+}
