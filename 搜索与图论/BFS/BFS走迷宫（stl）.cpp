1bbffbc03e54aa45a5ed4fc03fcaff25728370de
ZhuofanLi <z7074836@gmail.com>
2026-05-11 11:02:32 +0800

    Create BFS走迷宫（stl）.cpp

---
+++ 搜索与图论/BFS/BFS走迷宫（stl）.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> PLL;
+const int N=101;
+int s[N][N],d[N][N],n,m; //q[]表示地图，d[]表示经过时的步数
+int bfs()
+{
+    //从坐标（0,0）开始走
+    d[0][0]=0;
+    //开始手搓队列
+    queue<PLL>q;
+    q.push({0,0});
+    int dx[4]={-1,1,0,0} ,dy[4]={0,0,-1,1}; //dx,dy组合起来有上下左右效果
+    while(q.size())
+    {
+        auto item=q.front();
+        q.pop();
+        for(int i=0;i<4;i++)
+        {
+            int x=dx[i]+item.first,y=dy[i]+item.second;
+            if(x>=0 && x<n && y>=0 && y<m && d[x][y]==-1 && s[x][y]==0)
+            {
+                d[x][y]=d[item.first][item.second]+1;
+                if(x==n-1 && y==m-1) return d[x][y];
+                q.push({x,y});
+            }
+        }
+    }
+   
+    return d[n-1][m-1];
+}
+int main()
+{
+    cin>>n>>m;
+    memset(d,-1,sizeof d);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>s[i][j];
+        }
+    }
+    cout<<bfs()<<"\n";
+    return 0;
+}
